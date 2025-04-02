5a712a4af44cdc0a8e4dceb0d463daba5c033691
Asami Chiruko <asami.chiruko@gmail.com>
2025-04-03 08:47:19 +0900

    ESLintの設定

---
+++ eslint.config.js
@@ -0,0 +1,14 @@
+import { defineConfig } from "eslint/config";
+import globals from "globals";
+import js from "@eslint/js";
+import eslintConfigPrettier from "eslint-config-prettier";
+
+export default defineConfig([
+    { files: ["**/*.{js,mjs,cjs}"] },
+    {
+        files: ["**/*.{js,mjs,cjs}"],
+        languageOptions: { globals: { ...globals.browser, ...globals.node } }
+    },
+    { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"] },
+    eslintConfigPrettier
+]);
