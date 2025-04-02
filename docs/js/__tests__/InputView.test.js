f5ca5d45eba40480405b92a0e5477902640a0241
Asami Chiruko <asami.chiruko@gmail.com>
2025-04-02 17:04:35 +0900

    InputView, SettingsViewのテストコードを追加

---
+++ docs/js/__tests__/InputView.test.js
@@ -0,0 +1,61 @@
+import { InputView } from "../main.js";
+
+describe("InputView", () => {
+    let inputView;
+    let mockRecordViewModel;
+    let mockTabViewModel;
+    let textInput;
+    let saveButton;
+    let mockAlert;
+
+    beforeEach(() => {
+        document.body.innerHTML = `<textarea id="achievement"></textarea>
+        <button id="save-btn"></button>`;
+        textInput = document.getElementById("achievement");
+        saveButton = document.getElementById("save-btn");
+
+        mockAlert = vi.spyOn(globalThis, "alert").mockImplementation(() => {});
+        mockRecordViewModel = {
+            addAchievement: vi.fn(() => true)
+        };
+        mockTabViewModel = {
+            setActiveTab: vi.fn()
+        };
+        inputView = new InputView(mockRecordViewModel, mockTabViewModel);
+    });
+
+    afterEach(() => {
+        mockAlert.mockRestore();
+    });
+
+    test("入力内容があるときできたことを記録する", () => {
+        textInput.value = "test achievement";
+
+        saveButton.click();
+
+        expect(mockAlert).toHaveBeenCalled();
+        expect(mockRecordViewModel.addAchievement).toHaveBeenCalledWith({
+            content: "test achievement"
+        });
+    });
+
+    test("入力内容がないときそのことを知らせる", () => {
+        textInput.value = "";
+
+        saveButton.click();
+        expect(alert).toHaveBeenCalled();
+        expect(mockRecordViewModel.addAchievement).not.toHaveBeenCalled();
+    });
+
+    test("書き込み後に入力欄が空になる", () => {
+        textInput.value = "test achievement";
+        saveButton.click();
+        expect(textInput.value).toBe("");
+    });
+
+    test("書き込み後にリストタブがアクティブになる", () => {
+        textInput.value = "test achievement";
+        saveButton.click();
+        expect(mockTabViewModel.setActiveTab).toHaveBeenCalledWith("list");
+    });
+});
---
+++ docs/js/__tests__/SettingsView.test.js
@@ -0,0 +1,86 @@
+import { afterEach } from "vitest";
+import { SettingsView } from "../main.js";
+
+describe("SettingsView", () => {
+    let mockRecordViewModel;
+    let mockAlert;
+    let settingsView;
+
+    beforeEach(() => {
+        document.body.innerHTML = `
+        <input type="file" id="import-file"></input>
+        <button id="import-btn"></button>
+        <button id="export-btn"></button>
+        `;
+        mockRecordViewModel = {
+            importFromJsonString: vi.fn(() => true),
+            exportAsJsonString: vi.fn(() => "json object")
+        };
+        mockAlert = vi.spyOn(globalThis, "alert").mockImplementation(() => {});
+
+        settingsView = new SettingsView(mockRecordViewModel);
+    });
+
+    afterEach(() => {
+        mockAlert.mockRestore();
+    });
+
+    test("exportイベントでBlobとそのURLを生成してダウンロードを試みる", () => {
+        if (!globalThis.URL) {
+            globalThis.URL = {};
+        }
+        if (!URL.createObjectURL) {
+            URL.createObjectURL = vi.fn();
+        }
+        if (!URL.revokeObjectURL) {
+            URL.revokeObjectURL = vi.fn();
+        }
+        const mockCreateObjectURL = vi.spyOn(URL, "createObjectURL").mockReturnValue("mock-url");
+        const mockRevokeObjectURL = vi.spyOn(URL, "revokeObjectURL");
+
+        const a = document.createElement("a");
+        const mockCreateElement = vi.spyOn(document, "createElement").mockImplementation(() => a);
+        vi.spyOn(a, "click").mockImplementation();
+
+        settingsView.exportButton.click();
+
+        expect(a.href).toContain("mock-url");
+        expect(a.download).toBe("export_data.json");
+        expect(a.click).toHaveBeenCalled();
+
+        mockCreateObjectURL.mockRestore();
+        mockRevokeObjectURL.mockRestore();
+        mockCreateElement.mockRestore();
+    });
+
+    test("importイベントでfileが指定されているとき復元を試みる", async () => {
+        document.createElement("input");
+        const file = new File(["test file"], "test.json", { type: "application/json" });
+        if (!file.text) {
+            file.text = vi.fn();
+        }
+        vi.spyOn(file, "text").mockReturnValue("test file");
+        settingsView.importFile = {
+            files: [file]
+        };
+        await settingsView.importButton.click();
+
+        expect(mockRecordViewModel.importFromJsonString).toHaveBeenCalledWith("test file");
+        expect(file.text).toHaveBeenCalledWith();
+    });
+
+    test("importイベントでfileが正しく指定されていないときは復元しない", async () => {
+        document.createElement("input");
+        const file = new File(["test file"], "test.txt", { type: "text/plain" });
+        if (!file.text) {
+            file.text = vi.fn();
+        }
+        vi.spyOn(file, "text").mockReturnValue("test file");
+        settingsView.importFile = {
+            files: [file]
+        };
+        await settingsView.importButton.click();
+        expect(mockRecordViewModel.importFromJsonString).not.toHaveBeenCalled();
+        expect(file.text).not.toHaveBeenCalledWith();
+    });
+});
